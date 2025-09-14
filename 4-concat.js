d29fb2827a50f805bac3056051dc0ab6a2229645
Jesh-samso <samsonopondo1274@gmail.com>
2025-09-14 16:31:38 +0300

    Add script that prints two arguments in '<arg1> is <arg2>' format

--- 4-concat.js
+++ 4-concat.js
@@ -1,4 +1,6 @@
-let comment="is";
-console.log(`c ${comment} cool`);
-console.log(`c ${comment} undefined`);
-console.log(`undefined ${comment} undefined`)
+// 4-concat.js
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+console.log(arg1 + " is " + arg2);
